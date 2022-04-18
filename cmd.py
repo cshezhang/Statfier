f8faf7626b43b6e143c34d3b009d0b25975ca17d
HuaienZhang <austin.polyu@gmail.com>
2022-04-18 20:39:10 +0800

    update

--- cmd.py
+++ cmd.py
@@ -4,12 +4,9 @@ import subprocess
 def main():
     cmd = ["/bin/bash", "-c"]
     input_cmd = ""
-    f = open("res.log", "w")
     for i in range(1, len(sys.argv)):
       input_cmd = input_cmd + sys.argv[i] + " "
-    f.write(input_cmd)
     cmd.append(input_cmd)
     v = subprocess.call(cmd)
-    f.write("Python Exec: " + str(v))
-    f.close()
+
 main()
