bdc253c33ca97a83944155a4c1c00d6276d6aa48
HuaienZhang <cshezhang@comp.polyu.edu.hk>
2023-01-16 21:58:42 +0800

    add:time_log

--- src/main/java/org/rainyday/AutomaticTester.java
+++ src/main/java/org/rainyday/AutomaticTester.java
@@ -3,6 +3,10 @@ package org.rainyday;
 import org.rainyday.util.Schedule;
 import org.rainyday.util.Utility;
 
+import java.lang.management.ManagementFactory;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.rainyday.util.Schedule.writeEvaluationResult;
 import static org.rainyday.util.Utility.initEnv;
 import static org.rainyday.util.Utility.sourceSeedPath;
@@ -15,7 +19,13 @@ import static org.rainyday.util.Utility.sourceSeedPath;
 public class AutomaticTester {
 
     public static void main(String[] args) {
-        long time1 = System.currentTimeMillis();
+        long startTimeStamp = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String sd = sdf.format(new Date(Long.parseLong(String.valueOf(startTimeStamp))));
+        System.out.println("Start time: " + sd);
+        String name = ManagementFactory.getRuntimeMXBean().getName();
+        String pid = name.split("@")[0];
+        System.out.println("Java Program PID: " + pid);
         initEnv();
         Schedule schedule = Schedule.getInstance();
         if (Utility.PMD_MUTATION) {
@@ -34,10 +44,13 @@ public class AutomaticTester {
             schedule.executeSonarQubeTransform(sourceSeedPath);
         }
         writeEvaluationResult();
-        long OVERALL_EXEC_TIME = System.currentTimeMillis() - time1;
-        long minutes = (OVERALL_EXEC_TIME / 1000) / 60;
-        long seconds = (OVERALL_EXEC_TIME / 1000) % 60;
+        long endTimeStamp = System.currentTimeMillis();
+        long execTime = endTimeStamp - startTimeStamp;
+        long minutes = (execTime / 1000) / 60;
+        long seconds = (execTime / 1000) % 60;
         System.out.format("%d min(s) %d sec(s).", minutes, seconds);
+        sd = sdf.format(new Date(Long.parseLong(String.valueOf(endTimeStamp))));
+        System.out.println("End Time: " + sd);
     }
 
 }
