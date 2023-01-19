b80575d50d8e942aba0dc29f932e5e82521e8a96
HuaienZhang <cshezhang@comp.polyu.edu.hk>
2023-01-19 22:39:10 +0800

    update

--- seeds/SpotBugs_Seeds/IC_SUPERCLASS_USES_SUBCLASS_DURING_INITIALIZATION/CircularClassInitialization.java
+++
@@ -1,8 +0,0 @@
-public class CircularClassInitialization {
-  static class InnerClassSingleton extends CircularClassInitialization {
-    static InnerClassSingleton singleton = new InnerClassSingleton();
-  }
-
-  static CircularClassInitialization foo = InnerClassSingleton.singleton;
-}
-
--- src/main/java/org/detector/CleanSeeds.java
+++ src/main/java/org/detector/CleanSeeds.java
@@ -1,12 +1,20 @@
 package org.detector;
 
+import org.apache.commons.io.FileUtils;
 import org.detector.util.Utility;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import static org.detector.util.Invoker.invokeCommandsByZT;
+import static org.detector.util.Utility.SPOTBUGS_SEED_PATH;
+import static org.detector.util.Utility.getFilenamesFromFolder;
 import static org.detector.util.Utility.readFileByLine;
 
 /**
@@ -39,13 +47,53 @@ public class CleanSeeds {
         invokeCommandsByZT(curlCmd);
     }
 
+    public static void filterDuplicateSeedFiles(String seedFolderPath) {
+        List<String> filePaths = getFilenamesFromFolder(seedFolderPath, true);
+        HashMap<Integer, ArrayList<String>> hash2fileList = new HashMap<>();
+        for(String filePath : filePaths) {
+            if (!filePath.endsWith(".java")) {
+                System.out.println(filePath);
+            }
+        }
+        HashSet<Integer> fileHashSet = new HashSet<>();
+        for(String filePath : filePaths) {
+            File file = new File(filePath);
+            try {
+                String content = FileUtils.readFileToString(file, "UTF-8");
+                int hash = content.hashCode();
+                fileHashSet.add(hash);
+                if(!hash2fileList.containsKey(hash)) {
+                    ArrayList<String> pathList = new ArrayList<>();
+                    hash2fileList.put(hash, pathList);
+                }
+                hash2fileList.get(hash).add(filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Hash Size: " + fileHashSet.size());
+        System.out.println("List Size: " + filePaths.size());
+        for(ArrayList<String> pathList : hash2fileList.values()) {
+            if(pathList.size() > 1) {
+                for(int i = 1; i < pathList.size(); i++) {
+                    try {
+                        FileUtils.delete(new File(pathList.get(i)));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Set<String> ruleNames = getRuleNames();
-//        for(String ruleName : ruleNames) {
-//            invokeCurl(ruleName);
-//        }
-        System.out.println("Rule Size: " + ruleNames.size());
-        invokeCurl("null");
+        filterDuplicateSeedFiles(SPOTBUGS_SEED_PATH);
+//        Set<String> ruleNames = getRuleNames();
+////        for(String ruleName : ruleNames) {
+////            invokeCurl(ruleName);
+////        }
+//        System.out.println("Rule Size: " + ruleNames.size());
+//        invokeCurl("null");
     }
 
 }
